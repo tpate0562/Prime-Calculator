da0bc23727d47e8b1e1ef647405a32a4af530508
tpate0562 <tpate0562@stu.wusd.org>
2022-06-29 10:47:06 -0700

    Initial Commit

---
+++ Prime Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Prime Calculator
+//
+//  Created by Tejas Patel on 6/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
